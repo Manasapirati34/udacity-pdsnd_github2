599568c5246e96baca1e358e0dfebeb51a8b7f4a
Manasa Pirati <manasapirati@gmail.com>
2020-05-06 19:14:42 +0530

    refactor change 2

--- bikeshare.py
+++ bikeshare.py
@@ -25,7 +25,6 @@ def get_filters():
         city=(input())
         city=city.lower()
     # TO DO: get user input for month (all, january, february, ... , june)
-    #helo
     #month_list=['january','february','march','april','may','june','all']
     month=(input("enter month:"))
     month=month.lower()
